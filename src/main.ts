3022a670fb45bedcaa18f6900fc6c7cc5e363f52
Alex Gabites <alex@netspeed.net.nz>
2020-03-03 18:12:38 +1300

    review comments

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { context, GitHub } from '@actions/github'
+import {context, GitHub} from '@actions/github'
 import NetlifyAPI from 'netlify'
 import * as path from 'path'
 
@@ -7,43 +7,35 @@ async function run(): Promise<void> {
   try {
     const netlifyAuthToken = process.env.NETLIFY_AUTH_TOKEN
     const siteId = process.env.NETLIFY_SITE_ID
-
     // NOTE: Non-collaborators PRs don't pass GitHub secrets to GitHub Actions.
     if (!(netlifyAuthToken && siteId)) {
       process.stdout.write('Netlify credentials not provided, not deployable')
       return
     }
-
-    const dir = core.getInput('publish-dir', { required: true })
+    const dir = core.getInput('publish-dir', {required: true})
     const deployMessage = core.getInput('deploy-message') || undefined
     const productionBranch = core.getInput('production-branch')
-
     // NOTE: if production-branch input is not specified, it is "", so isDraft is always true
     const isDraft: boolean = context.ref !== `refs/heads/${productionBranch}`
 
     // Create Netlify API client
     const netlifyClient = new NetlifyAPI(netlifyAuthToken)
-
     // Resolve publish directory
     const deployFolder = path.resolve(process.cwd(), dir)
-
     // Deploy to Netlify
     const deploy = await netlifyClient.deploy(siteId, deployFolder, {
       draft: isDraft,
       message: deployMessage
     })
-
     // Create a message
     const message = isDraft
       ? `🚀 Deployed on ${deploy.deploy.deploy_ssl_url}`
       : `🎉 Published on ${deploy.deploy.ssl_url} as production\n🚀 Deployed on ${deploy.deploy.deploy_ssl_url}`
-
     // Print the URL
     process.stdout.write(`${message}\n`)
 
     // Get GitHub token
     const githubToken = core.getInput('github-token')
-
     if (githubToken !== '') {
       // Create GitHub client
       const githubClient = new GitHub(githubToken)
@@ -51,23 +43,24 @@ async function run(): Promise<void> {
       const commitCommentParams = {
         owner: context.repo.owner,
         repo: context.repo.repo,
+        // eslint-disable-next-line @typescript-eslint/camelcase
         commit_sha: context.sha,
         body: message
       }
-
       // TODO: Remove try/catch block
       // NOTE: try-catch is experimentally used because commit message may not be done in some conditions.
       try {
         // Comment to the commit
         await githubClient.repos.createCommitComment(commitCommentParams)
       } catch (err) {
+        // eslint-disable-next-line no-console
         console.error(err, JSON.stringify(commitCommentParams, null, 2))
       }
-
       // If it is a pull request
       if (context.issue.number !== undefined) {
         // Comment the deploy URL
         await githubClient.issues.createComment({
+          // eslint-disable-next-line @typescript-eslint/camelcase
           issue_number: context.issue.number,
           owner: context.repo.owner,
           repo: context.repo.repo,
