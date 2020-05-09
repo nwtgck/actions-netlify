f7099b1a080022b7bdd83fe122ff95f7bcd806b0
Ryo Ota <nwtgck@nwtgck.org>
2020-05-09 11:19:13 +0900

    refactor: inject inputs to run()

---
+++ src/inputs.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core'
+
+// Why use function rather than raw string? => Inputs should be lazy evaluated.
+export interface Inputs {
+  publishDir(): string
+  deployMessage(): string | undefined
+  productionBranch(): string | undefined
+  enablePullRequestComment(): boolean
+  enableCommitComment(): boolean
+  githubToken(): string
+}
+
+export const defaultInputs: Inputs = {
+  publishDir() {
+    return core.getInput('publish-dir', {required: true})
+  },
+  deployMessage() {
+    return core.getInput('deploy-message') || undefined
+  },
+  productionBranch() {
+    return core.getInput('production-branch') || undefined
+  },
+  enablePullRequestComment() {
+    // Default: true
+    return (core.getInput('enable-pull-request-comment') || 'true') === 'true'
+  },
+  enableCommitComment() {
+    // Default: true
+    return (core.getInput('enable-commit-comment') || 'true') === 'true'
+  },
+  githubToken() {
+    return core.getInput('github-token')
+  }
+}
