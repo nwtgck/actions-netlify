b07d982378aea91d1a80e2903a40219abbe32353
Ryo Ota <nwtgck@nwtgck.org>
2020-05-10 11:48:43 +0900

    trigger push

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -128,6 +128,7 @@ test('throws invalid number', async () => {
 test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
+  
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
