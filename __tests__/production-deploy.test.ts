fa9f1ec5676b18136f4fad185f075deae30098d9
Ryo Ota <nwtgck@nwtgck.org>
2020-06-13 23:32:28 +0900

    use ts-jest mocked() instead of type assertion

--- __tests__/production-deploy.test.ts
+++ __tests__/production-deploy.test.ts
@@ -1,8 +1,8 @@
 import * as path from 'path'
+import { mocked } from 'ts-jest/utils'
 import {defaultInputs} from '../src/inputs'
 import {context} from '@actions/github'
 import {run} from '../src/main'
-import Mock = jest.Mock
 
 const mockDeploy = jest.fn()
 
@@ -15,8 +15,8 @@ jest.mock('../src/inputs')
 jest.mock('@actions/github')
 
 mockDeploy.mockResolvedValue({deploy: {}})
-;(<Mock<any>>defaultInputs.githubToken).mockReturnValue('')
-;(<Mock<any>>defaultInputs.publishDir).mockReturnValue('publish-dir')
+mocked(defaultInputs.githubToken).mockReturnValue('')
+mocked(defaultInputs.publishDir).mockReturnValue('publish-dir')
 
 process.env.NETLIFY_AUTH_TOKEN = 'auth-token'
 process.env.NETLIFY_SITE_ID = 'site-id'
@@ -25,7 +25,7 @@ describe('Draft deploy', () => {
   const expectedDeployFolder = path.resolve(process.cwd(), 'publish-dir')
 
   test('deploy should have draft true when production-deploy input is false', async () => {
-    ;(<Mock<any>>defaultInputs.productionDeploy).mockReturnValue(false)
+    mocked(defaultInputs.productionDeploy).mockReturnValue(false)
 
     await run(defaultInputs)
 
@@ -35,7 +35,7 @@ describe('Draft deploy', () => {
   })
 
   test('deploy should have draft false when production-deploy input is true', async () => {
-    ;(<Mock<any>>defaultInputs.productionDeploy).mockReturnValue(true)
+    mocked(defaultInputs.productionDeploy).mockReturnValue(true)
 
     await run(defaultInputs)
 
@@ -45,8 +45,8 @@ describe('Draft deploy', () => {
   })
 
   test('deploy should have draft false when production-branch matches context ref', async () => {
-    ;(<Mock<any>>defaultInputs.productionDeploy).mockReturnValue(false)
-    ;(<Mock<any>>defaultInputs.productionBranch).mockReturnValue('master')
+    mocked(defaultInputs.productionDeploy).mockReturnValue(false)
+    mocked(defaultInputs.productionBranch).mockReturnValue('master')
 
     context.ref = 'refs/heads/master'
 
@@ -58,8 +58,8 @@ describe('Draft deploy', () => {
   })
 
   test('deploy should have draft true when production-branch doesnt match context ref', async () => {
-    ;(<Mock<any>>defaultInputs.productionDeploy).mockReturnValue(false)
-    ;(<Mock<any>>defaultInputs.productionBranch).mockReturnValue('master')
+    mocked(defaultInputs.productionDeploy).mockReturnValue(false)
+    mocked(defaultInputs.productionBranch).mockReturnValue('master')
 
     context.ref = 'refs/heads/not-master'
 
@@ -71,8 +71,8 @@ describe('Draft deploy', () => {
   })
 
   test('deploy should have draft true when production-branch is not defined', async () => {
-    ;(<Mock<any>>defaultInputs.productionDeploy).mockReturnValue(false)
-    ;(<Mock<any>>defaultInputs.productionBranch).mockReturnValue(undefined)
+    mocked(defaultInputs.productionDeploy).mockReturnValue(false)
+    mocked(defaultInputs.productionBranch).mockReturnValue(undefined)
 
     context.ref = 'refs/heads/master'
 
