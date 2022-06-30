6e53e94eeea38dd9238d72e87893993965f82152
Ryo Ota <nwtgck@nwtgck.org>
2022-06-30 19:33:18 +0900

    eslint-disable-next-line @typescript-eslint/no-explicit-any

--- src/main.ts
+++ src/main.ts
@@ -235,6 +235,7 @@ export async function run(inputs: Inputs): Promise<void> {
         console.error(err)
       }
     }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     core.setFailed(error.message)
   }
